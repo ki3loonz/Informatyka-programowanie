cadbedb18dbf1e932a423eac651a07022f39c829
ki3loonz <dobakszymon@gmail.com>
2025-11-20 11:04:16 +0100

    zadania

---
+++ Pętla while i do-while/zadanie 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Podaj liczbe N: ";
+    cin >> N;
+
+    cout << "Liczby parzyste od 2 do " << N << ":" << endl;
+
+    int i = 2;
+    while (i <= N) {
+        cout << i << endl;
+        i += 2;
+    }
+
+    return 0;
+}
